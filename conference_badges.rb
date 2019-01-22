6fe9ff8b5816d47a7fc8550a97fee0ab5a7057ae
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 00:34:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,13 @@ def batch_badge_creator(names)
   end
   return badge_array
 end
-  
+
+def assign_rooms(names)
+  room_array = []
+  counter = 1
+  names.each do |name|
+    room_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter +=1
+  end
+  return room_array
+end
