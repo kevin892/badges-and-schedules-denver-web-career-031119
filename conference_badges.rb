65547df5d6a9167e52777d9025840144a61a372f
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 00:21:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,17 @@ def badge_maker(name)
   puts "Hello, my name is #{{name}}."
 end
 
-def batch_badge_creator (names)
+def batch_badge_creator (speakers)
+  speakers.collect {|name|
+    badge_maker(name)
+  }
+end
 
 
-def assign_rooms
+def assign_rooms(speakers)
+  room = 0
+  speakers.collect {|name|
+    room += 1
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+  }
+end
